d4e220b32238c5211605616ea9f95e726b54ba02
Andrey Popp <8mayday@gmail.com>
2014-04-17 00:31:42 +0600

    Field: allow injecting label and input via props

--- tests/Field.js
+++ tests/Field.js
@@ -32,7 +32,10 @@ describe('forms', () => {
       },
 
       schema: function() {
-        return schema.Property();
+        return (
+          this.props.schema ||
+          schema.Property()
+        );
       },
 
       valueLens: function() {
@@ -52,26 +55,79 @@ describe('forms', () => {
       onValueUpdate = sinon.spy();
     });
 
-    it('renders into <input /> by default', () => {
-      var field = Field();
-      var form = Form({field});
-      form = ReactTestUtils.renderIntoDocument(form);
-      var input = ReactTestUtils.findRenderedDOMComponentWithTag(form, 'input');
-      assert.equal(input.getDOMNode().value, 42);
-    });
-
     it('reacts on onChange event', () => {
-      var field = Field();
-      var form = Form({field});
+      var form = Form({field: Field()});
       form = ReactTestUtils.renderIntoDocument(form);
       var input = ReactTestUtils.findRenderedDOMComponentWithTag(form, 'input');
       input.getDOMNode().value = 'newvalue';
       ReactTestUtils.Simulate.change(input);
-      assert.equal(onValueUpdate.callCount, 1);
+      assert.strictEqual(onValueUpdate.callCount, 1);
       assert.deepEqual(onValueUpdate.firstCall.args, [
         'newvalue',
         {validation: {failure: undefined}, isSuccess: true, isFailure: false}
       ]);
+    });
+
+    describe('input rendering', () => {
+
+      it('renders into <input /> by default', () => {
+        var form = Form({field: Field()});
+        form = ReactTestUtils.renderIntoDocument(form);
+        var input = ReactTestUtils.findRenderedDOMComponentWithTag(form, 'input');
+        assert.strictEqual(input.getDOMNode().value, '42');
+      });
+
+      it('renders into component injected via schema', () => {
+        var field = Field();
+        var form = Form({
+          field: Field(),
+          schema: schema.Property({input: <textarea />})
+        });
+        form = ReactTestUtils.renderIntoDocument(form);
+        var input = ReactTestUtils.findRenderedDOMComponentWithTag(form, 'textarea');
+        assert.strictEqual(input.getDOMNode().value, '42');
+      });
+
+      it('renders into component injected via props', () => {
+        var field = Field();
+        var form = Form({
+          field: Field({input: <textarea />})
+        });
+        form = ReactTestUtils.renderIntoDocument(form);
+        var input = ReactTestUtils.findRenderedDOMComponentWithTag(form, 'textarea');
+        assert.strictEqual(input.getDOMNode().value, '42');
+      });
+    });
+
+    describe('label rendering', () => {
+
+      function assertLabel(form, value) {
+        var label = ReactTestUtils.findRenderedDOMComponentWithTag(form, 'label');
+        assert.strictEqual(label.getDOMNode().innerHTML, value);
+      }
+
+      it('renders no label if no label is provided via schema or props', () => {
+        var form = ReactTestUtils.renderIntoDocument(Form({
+          field: Field()
+        }));
+        var labels = ReactTestUtils.scryRenderedDOMComponentsWithTag(form, 'label');
+        assert.strictEqual(labels.length, 0);
+      });
+
+      it('renders label via schema', () => {
+        var form = ReactTestUtils.renderIntoDocument(Form({
+          field: Field(),
+          schema: schema.Property({label: 'label via schema'})
+        }));
+        assertLabel(form, 'label via schema');
+      });
+
+      it('renders label passed via props', () => {
+        var form = ReactTestUtils.renderIntoDocument(Form({
+          field: Field({label: 'label via props'})
+        }));
+        assertLabel(form, 'label via props');
+      });
     });
   });
 });
