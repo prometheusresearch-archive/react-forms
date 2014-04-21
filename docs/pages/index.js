4b3a953e0a03a65915b93e5ccfe8a2b3b7859a69
Andrey Popp <8mayday@gmail.com>
2014-04-21 16:32:59 +0400

    docs: gardening

--- docs/pages/index.js
+++ docs/pages/index.js
@@ -16,6 +16,19 @@ module.exports = React.createClass({
             process forms. It is designed to handle arbitraty complex data
             structures and to be customizable and extendable at any level.
           </p>
+          <p>
+            This package is a part of the <a href="http://rexdb.org">RexDB</a>®
+            platform for medical research data management. RexDB is free
+            software created by <a href="http://prometheusresearch.com">Prometheus
+            Research, LLC</a> and is released under the AGPLv3 license with a
+            commensurate attribution clause. For more information, please visit
+            <a href="http://rexdb.org/">http://rexdb.org/</a>.
+          </p>
+          <p>
+            The development of this product was supported by the National
+            Institute of Mental Health of the National Institutes of Health
+            under Award Number R43MH099826.
+          </p>
         </Section>
       </Demo>
     );
