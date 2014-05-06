fc0deaeae2e3833eb4dd172800252b6d26a5af0e
Andrey Popp <8mayday@gmail.com>
2014-05-06 12:28:36 +0400

    Use only public React API
    Apart from the fact that using public API is better we also could keep using the
    same codebase with react builds from bower.

--- standalone/index.js
+++ standalone/index.js
@@ -5,46 +5,5 @@
     root.ReactForms = factory(root.React);
   }
 })(window, function(React) {
-
-  var __ReactShim = window.__ReactShim = window.__ReactShim || {};
-
-  __ReactShim.React = React;
-
-  __ReactShim.cloneWithProps = React.addons.cloneWithProps;
-
-  __ReactShim.cx = React.addons.classSet;
-
-  __ReactShim.invariant = function(check, msg) {
-    if (!check) {
-      throw new Error(msg);
-    }
-  }
-
-  var mergeInto = __ReactShim.mergeInto = function(dst, src) {
-    for (var k in src) {
-      if (src.hasOwnProperty(k)) {
-        dst[k] = src[k];
-      }
-    }
-  }
-
-  __ReactShim.merge = function(a, b) {
-    var c = {};
-    mergeInto(c, a);
-    mergeInto(c, b);
-    return c;
-  }
-
-  __ReactShim.emptyFunction = function() {
-  }
-
-  __ReactShim.emptyFunction.thatReturnsTrue = function() {
-    return true;
-  }
-
-  __ReactShim.ReactUpdates = {
-    batchedUpdates: function(cb) { cb(); }
-  };
-
   return require('./lib/');
 });
