bfd935ffddd3b2cae743d28e69ad3d1d00f3767d
Andrey Popp <8mayday@gmail.com>
2014-05-30 13:44:38 +0400

    Add basic integration tests

--- Makefile
+++ Makefile
@@ -1,6 +1,7 @@
 BIN   = ./node_modules/.bin
 PATH := $(BIN):$(PATH)
 TESTS = $(shell find ./lib -path '**/__tests__/*.js')
+INTEGRATION_TESTS = $(shell find ./tests -name '*.js')
 MAKE_DOCS = $(MAKE) --no-print-directory -C docs
 
 lint:
@@ -9,10 +10,16 @@ lint:
 clean:
 	@rm -rf ./node_modules/
 
-test: test-phantomjs
+test: test-phantomjs integration-test
+
+integration-ci:
+	@NODE_PATH=$(NODE_PATH) mochify --watch -R dot $(INTEGRATION_TESTS)
+
+integration-test:
+	@NODE_PATH=$(NODE_PATH) mochify -R dot $(INTEGRATION_TESTS)
 
 ci:
 	@NODE_PATH=$(NODE_PATH) mochify --watch -R dot $(TESTS)
 
 test-phantomjs:
-	@NODE_PATH=$(NODE_PATH) mochify -R spec $(TESTS)
+	@NODE_PATH=$(NODE_PATH) mochify -R dot $(TESTS)
---
+++ tests/simple.js
@@ -0,0 +1,98 @@
+/**
+ * @jsx React.DOM
+ */
+'use strict';
+
+var sinon = require('sinon');
+var assert = require('assert');
+
+var ReactForms = require('../');
+var React = require('react');
+var TestUtils = require('react/lib/ReactTestUtils');
+
+var Property = ReactForms.schema.Property;
+var Schema = ReactForms.schema.Schema;
+
+var Form = ReactForms.Form;
+var Field = ReactForms.Field;
+var Fieldset = ReactForms.Fieldset;
+
+describe('simple form integration test', function() {
+
+  var schema = (
+    <Schema>
+      <Property name="text" />
+      <Property name="num" type="number" />
+    </Schema>
+  )
+
+  var form;
+  var onChange;
+  var onUpdate;
+  var fields;
+  var inputs;
+
+  beforeEach(function() {
+    onChange = sinon.spy();
+    onUpdate = sinon.spy();
+    form = TestUtils.renderIntoDocument(Form({schema, onChange, onUpdate}));
+    fields = {};
+    inputs = {};
+    TestUtils.scryRenderedComponentsWithType(form, Field).forEach(function(field) {
+      fields[field.value().name] = field;
+      inputs[field.value().name] = TestUtils.findRenderedDOMComponentWithTag(field, 'input');
+    });
+  });
+
+  it('renders', function() {
+    assert.ok(fields.text);
+    assert.ok(fields.num);
+    assert.ok(inputs.text);
+    assert.ok(inputs.num);
+
+    assert.equal(inputs.text.getDOMNode().value, '');
+    assert.equal(inputs.num.getDOMNode().value, '');
+  });
+
+  it('has empty value initially', function() {
+    assert.deepEqual(form.value().value, {});
+    assert.deepEqual(form.value().serialized, {});
+    assert.ok(ReactForms.validation.isSuccess(form.value().validation));
+  });
+
+  it('updates value on user input', function() {
+    TestUtils.Simulate.change(inputs.text, {target: {value: 'text!'}});
+    assert.deepEqual(form.value().value, {text: 'text!'});
+    assert.deepEqual(form.value().serialized, {text: 'text!'});
+    assert.ok(ReactForms.validation.isSuccess(form.value().validation));
+
+    TestUtils.Simulate.change(inputs.num, {target: {value: '42'}});
+    assert.deepEqual(form.value().value, {text: 'text!', num: 42});
+    assert.deepEqual(form.value().serialized, {text: 'text!', num: '42'});
+    assert.ok(ReactForms.validation.isSuccess(form.value().validation));
+  });
+
+  it('updates value on invalid user input', function() {
+    TestUtils.Simulate.change(inputs.num, {target: {value: 'invalid'}});
+    assert.deepEqual(form.value().value, {num: 'invalid'});
+    assert.deepEqual(form.value().serialized, {num: 'invalid'});
+    assert.ok(ReactForms.validation.isFailure(form.value().validation));
+  });
+
+  it('fires callbacks on user input', function() {
+    TestUtils.Simulate.change(inputs.text, {target: {value: 'text!'}});
+    assert.equal(onUpdate.callCount, 1);
+    assert.equal(onChange.callCount, 1);
+
+    TestUtils.Simulate.change(inputs.num, {target: {value: '42'}});
+    assert.equal(onUpdate.callCount, 2);
+    assert.equal(onChange.callCount, 2);
+  });
+
+  it('fires callbacks on invalid user input', function() {
+    TestUtils.Simulate.change(inputs.num, {target: {value: 'invalid!'}});
+    assert.equal(onUpdate.callCount, 1);
+    assert.equal(onChange.callCount, 0);
+  });
+
+});
