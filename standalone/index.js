186a1cbcef79161fd35ec4f592bb1bcc335ab4d2
Andrey Popp <8mayday@gmail.com>
2014-12-01 12:05:27 +0800

    Remove standalone/

--- standalone/index.js
+++
@@ -1,9 +0,0 @@
-;(function (root, factory) {
-  if (typeof define === 'function' && define.amd) {
-    define(['react'], factory);
-  } else {
-    root.ReactForms = factory(root.React);
-  }
-})(window, function(React) {
-  return require('./lib/');
-});
