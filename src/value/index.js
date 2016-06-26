e84fe7682d28fcffc895cca7aab2aca7989b6253
Andrey Popp <8mayday@gmail.com>
2016-06-26 15:03:01 +0300

    fix(value): fix exports

--- src/value/index.js
+++ src/value/index.js
@@ -2,17 +2,19 @@
  * @copyright 2016-present, Prometheus Research, LLC
  */
 
+import withFormValue from '../withFormValue';
+
 export {validate} from '../Schema';
 export Fieldset from '../Fieldset';
 export * as Schema from '../Schema';
 export Input from '../Input';
 
 export ErrorList from './ErrorList';
-export withFormValue from '../withFormValue';
 export {createValue, suppressUpdate} from './Value';
+export withFormValue from '../withFormValue';
 export Field from './Field';
 
 export function WithFormValue(...args) {
-  console.error('WithFormValue(..) is renamed to withFormValue(..)');
+  console.error('WithFormValue(..) is renamed to withFormValue(..)'); // eslint-disable-line no-console
   return withFormValue(...args);
 }
